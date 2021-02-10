04e98442c3e28925211420258db4461324e59c4c
FrontAlbert <alberthui@outlook.com>
2021-02-09 21:56:23 -0500

    6. More Styling

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -8,6 +8,7 @@ const GlobalStyle = createGlobalStyle`
 }
 
 body{
+    font-family:'Inter', sans-serif;
     background:#1b1b1b
 }
 
@@ -20,6 +21,7 @@ button{
     background:transparent;
     color:white;
     transition:all0.5s ease;
+    font-family:'Inter', sans-serif;
     &:hover{
         background-color:  #23d997;
         color:white;
