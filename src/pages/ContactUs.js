4108c1d62a90ea61cd8f1f14a9e5ea9b56647e10
FrontAlbert <alberthui@outlook.com>
2021-02-12 18:29:42 -0500

    25. Finish Touches

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -52,6 +52,11 @@ const ContactStyle = styled(motion.div)`
     padding: 5rem 10rem;
     color: #353535;
     min-height: 90vh;
+    @media (max-width: 1500px) {
+        margin-top: 5rem;
+        padding: 2rem;
+        font-size: 1rem;
+    }
 `;
 
 const Title = styled.div`
