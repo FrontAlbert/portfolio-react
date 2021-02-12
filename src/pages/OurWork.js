2f4b25250fae91a3784ada946b3e0811a9a1bc81
FrontAlbert <alberthui@outlook.com>
2021-02-12 01:10:47 -0500

    18 more fade for ourWork

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -16,8 +16,11 @@ import {
     // slider,
     // sliderContainer,
 } from "../animation";
+import { useScroll } from "../components/useScroll";
 
 const OurWork = () => {
+    const [elements, controls] = useScroll();
+    const [elements2, controls2] = useScroll();
     return (
         <Work
             variants={pageAnimation}
@@ -47,14 +50,24 @@ const OurWork = () => {
                     </Hide>
                 </Link>
             </Movie>
-            <Movie>
+            <Movie
+                ref={elements}
+                variants={fade}
+                animate={controls}
+                initial="hidden"
+            >
                 <h2>The Racer</h2>
                 <div className="line"></div>
                 <Link to="/work/the-racer">
                     <img src={theracer} alt="theracer" />
                 </Link>
             </Movie>
-            <Movie>
+            <Movie
+                ref={elements2}
+                variants={fade}
+                animate={controls2}
+                initial="hidden"
+            >
                 <h2>Good Times </h2>
                 <div className="line"></div>
                 <Link to="/work/good-times">
@@ -76,7 +89,7 @@ const Work = styled(motion.div)`
         padding: 1rem 0rem;
     }
 `;
-const Movie = styled.div`
+const Movie = styled(motion.div)`
     padding-bottom: 10rem;
     .line {
         height: 0.5rem;
